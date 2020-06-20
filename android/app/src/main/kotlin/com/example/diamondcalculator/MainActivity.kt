65f82e4458d0a2becd3fea4332cc4779d4eafd1a
parmeshS <parmesh@signy.io>
2020-06-21 02:02:22 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/diamondcalculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diamondcalculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+
+const kBackgroudColor = Color(0xFF282B2F);
+const kPrimaryColor = Color(0xFF6E6F76);
+const kPrimaryDarkColor = Color(0xFF0C0E10);
+const kAccentColor = Color(0xFF5E7AF4);
